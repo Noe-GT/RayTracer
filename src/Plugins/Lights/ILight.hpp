aafb16995cee4134d6115b05b465435b43f9c52a
nicolasnny <nicolas.nunney@epitech.eu>
2025-04-24 09:21:40 +0200

    feat: ILight interface template

---
+++ src/Plugins/Lights/ILight.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2025
+** RayTracer
+** File description:
+** ILight
+*/
+
+#pragma once
+
+class ILight {
+    public:
+        ILight();
+        ~ILight();
+
+    protected:
+    private:
+};
