123f6c6c4d45874a68e0fae8f1eb564ded131434
Noe Gebert <noe-daniel.gebert@epitech.com>
2025-05-08 12:54:21 +0200

    refactor: more efficient parser

--- src/core/src/Parser.cpp
+++ src/core/src/Parser.cpp
@@ -17,19 +17,17 @@ void rayTracer::Parser::loadConfig(const std::string &filePath, rayTracer::RayTr
 {
     try {
         config.readFile(filePath.c_str());
-        if (config.exists("primitives"))
         this->parsePrimitives(rayTracer.getScene());
-        if (config.exists("camera"))
-            this->parseCamera(rayTracer.getScene());
-        if (config.exists("image"))
-            this->parseImage(rayTracer);
-        if (config.exists("graphical"))
-            this->parseGraphical(rayTracer);
+        this->parseCamera(rayTracer.getScene());
+        this->parseImage(rayTracer);
+        this->parseGraphical(rayTracer);
     } catch (const libconfig::FileIOException &fioex) {
         throw rayTracer::ConfigException("I/O error while reading file.");
     } catch (const libconfig::ParseException &pex) {
         throw rayTracer::ConfigException("Parse error at " + std::string(pex.getFile()) + ":" +
             std::to_string(pex.getLine()) + " - " + pex.getError());
+    } catch (const libconfig::SettingNotFoundException &e) {
+        return;
     }
 }
 
@@ -41,7 +39,7 @@ void rayTracer::Parser::parsePrimitives(rayTracer::Scene &scene)
     for (std::pair<std::string, rayTracer::PluginHandler::Plugin<IPrimitive>> pPair : pPlugins) {
         if (primitives.exists(pPair.first + "s")) {
             scene._obj.push_back(pPair.second.getFactory()->build());
-            scene._obj.back()->configure(primitives.lookup(pPair.first + "s"));
+            scene._obj.back()->configure(primitives[(pPair.first + "s").c_str()]);
         }
     }
 }
@@ -49,13 +47,12 @@ void rayTracer::Parser::parsePrimitives(rayTracer::Scene &scene)
 void rayTracer::Parser::parseGraphical(rayTracer::RayTracer &rayTracer)
 {
     const libconfig::Setting &graphicalConf = config.lookup("graphical");
+    std::string libraryName = graphicalConf.lookup("library");
     const std::map<std::string, rayTracer::PluginHandler::Plugin<IGraphical>> &gPlugins = this->_pluginHandler.getGraphicalPlugins();
     std::pair<size_t, size_t> res = rayTracer.getImageResolution();
 
-    if (!graphicalConf.exists("library"))
-        return;
     for (std::pair<std::string, rayTracer::PluginHandler::Plugin<IGraphical>> gPair : gPlugins) {
-        if (static_cast<std::string>(graphicalConf["library"]) == gPair.first) {
+        if (libraryName == gPair.first) {
             std::shared_ptr<IGraphical> graphical = gPair.second.getFactory()->build(res.first, res.second);
             rayTracer.setGraphical(graphical);
             return;
@@ -66,36 +63,28 @@ void rayTracer::Parser::parseGraphical(rayTracer::RayTracer &rayTracer)
 void rayTracer::Parser::parseImage(rayTracer::RayTracer &rayTracer)
 {
     const libconfig::Setting &imgConf = config.lookup("image");
-    int width;
-    int height;
+    const libconfig::Setting &resConf = imgConf.lookup("resolution");
+    int width = resConf.lookup("width");
+    int height = resConf.lookup("height");
 
-    if (imgConf.exists("resolution")) {
-        const libconfig::Setting &resConf = imgConf.lookup("resolution");
-        if (resConf.exists("width") && resConf.exists("height")) {
-            width = imgConf["resolution"]["width"];
-            height = imgConf["resolution"]["height"];
-            if (width < 0)
-                width = 0;
-            if (height < 0)
-                height = 0;
-            rayTracer.setImage(std::pair<size_t, size_t>(width, height));
-        }
-    }
+    if (width < 0)
+        width = 0;
+    if (height < 0)
+        height = 0;
+    rayTracer.setImage(std::pair<size_t, size_t>(width, height));
 }
 
 void rayTracer::Parser::parseCamera(rayTracer::Scene &scene)
 {
     const libconfig::Setting &cameraConf = config.lookup("camera");
 
-    if (cameraConf.exists("position")) {
+    try {
         const libconfig::Setting &posConf = cameraConf.lookup("position");
-        if (posConf.exists("x") && posConf.exists("y") && posConf.exists("z")) {
-            scene._camera.setPosition(math::Point(
-                static_cast<double>(static_cast<int>(posConf["x"])),
-                static_cast<double>(static_cast<int>(posConf["y"])),
-                static_cast<double>(static_cast<int>(posConf["z"]))));
-        }
-    }
+        double x = posConf.lookup("x");
+        double y = posConf.lookup("y");
+        double z = posConf.lookup("z");
+        scene._camera.setPosition(math::Point(x, y, z));
+    } catch (const libconfig::SettingNotFoundException &e) {}
     if (cameraConf.exists("fieldOfView")) {
         scene._camera.setFov(cameraConf["fieldOfView"]);
     }
